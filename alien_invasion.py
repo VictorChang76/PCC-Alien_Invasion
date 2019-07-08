7a0435ef8b67e634f7d8d5984cd530df389e0dc4
VictorChang76 <43318235+VictorChang76@users.noreply.github.com>
2019-07-08 23:23:42 +0800

    Reset the game after click Play and deactivate the play button when game is in a active state.

--- alien_invasion.py
+++ alien_invasion.py
@@ -24,7 +24,7 @@ class AlienInvasion:
         self.bullets = pygame.sprite.Group()
         self.aliens = pygame.sprite.Group()
 
-        self._create_fleet()
+        # self._create_fleet()
 
         # Make the play button
         self.play_button = Button(self, "Play")
@@ -56,8 +56,19 @@ class AlienInvasion:
 
     def _check_play_button(self, mouse_pos):
         """Start a new game when the player clicks Play."""
-        if self.play_button.rect.collidepoint(mouse_pos):
+        button_clicked = self.play_button.rect.collidepoint(mouse_pos)
+        if button_clicked and not self.stats.game_active:
+            # Reset the game statistics.
+            self.stats.reset_stats()
             self.stats.game_active = True
+
+            # Get rid of any remaining aliens and bullets.
+            self.aliens.empty()
+            self.bullets.empty()
+
+            # Create a new fleet and center the ship.
+            self._create_fleet()
+            self.ship.center_ship()
 
     def _check_keydown_events(self, event):
         """Respond to keypresses."""
